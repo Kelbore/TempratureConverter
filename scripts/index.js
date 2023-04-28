075beaeb5f0194ff0058fe2f31b2f893f27a428f
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-27 20:01:08 -0400

    modified html and javascript files

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,22 @@ window.onload = init;
 function init() {
     const converterBtn = document.getElementById("converterBtn");
     converterBtn.onclick = onConverterBtnClicked;
+    const resetBtn = document.getElementById("resetBtn");
+    resetBtn.onclick = resetInputField;
 }
 
     const celsiusTemp = document.getElementById("celsiusTemp");
     const tempInFahrenheit = document.getElementById("tempInFahrenheit");
+
 
 function  onConverterBtnClicked() {
     const temp = Number(celsiusTemp.value);
     const fahrenheit = (1.8*temp) + 32;
     tempInFahrenheit.value = fahrenheit;
 }     
+
+function resetInputField() {
+    document.getElementById("myForm").reset();
+}
 
   
