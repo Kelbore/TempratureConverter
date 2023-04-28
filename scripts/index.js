ed3bb27f91e795ea38e6dee0234758de9578a702
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-28 10:57:12 -0400

    modified html and javascript files

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,13 @@ window.onload = init;
 
 function init() {
     const converterBtn = document.getElementById("converterBtn");
-    converterBtn.onclick = onConverterBtnClicked;
+    if(converterBtn != null) {
+      converterBtn.onclick = onConverterBtnClicked;
+    }
+    const converterBtnF = document.getElementById("converterBtnF");
+    if(converterBtnF != null) {
+      converterBtnF.onclick = fahrenheitBtnClicked;
+    }
     const resetBtn = document.getElementById("resetBtn");
     resetBtn.onclick = resetInputField;
 }
@@ -12,12 +18,21 @@ function init() {
     const celsiusTemp = document.getElementById("celsiusTemp");
     const tempInFahrenheit = document.getElementById("tempInFahrenheit");
 
+    const fahrenTemp = document.getElementById("fahrenTemp");
+    const tempInCelsius = document.getElementById("tempInCelsius");
 
 function  onConverterBtnClicked() {
     const temp = Number(celsiusTemp.value);
     const fahrenheit = (1.8*temp) + 32;
     tempInFahrenheit.value = fahrenheit;
 }     
+
+function fahrenheitBtnClicked() {
+    const fahrenheitValue = Number(fahrenTemp.value);
+    const x = 5/9;
+    const answer = (fahrenheitValue - 32) * x;
+    tempInCelsius.value = answer;
+}
 
 function resetInputField() {
     document.getElementById("myForm").reset();
